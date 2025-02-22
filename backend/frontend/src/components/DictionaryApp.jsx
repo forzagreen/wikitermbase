fc459ce1c4fa4a709b825087f28b7ceccf6bf478
wael <8544289+forzagreen@users.noreply.github.com>
2025-02-22 01:11:07 +0100

    UI: minor: suggested translation

--- backend/frontend/src/components/DictionaryApp.jsx
+++ backend/frontend/src/components/DictionaryApp.jsx
@@ -325,7 +325,7 @@ const DictionaryApp = () => {
                 </div>
                 {isTopResult && (
                   <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
-                    التعريب المقترح
+                    الترجمة المقترحة
                   </span>
                 )}
                 <div dir="ltr" className="flex-1 text-left">
